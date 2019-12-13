d4f33c128655d755ec56030d38952cc2f7a27101
BrunoJusta <brunomajusta@gmail.com>
2019-12-13 13:45:43 +0000

    gg

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -29,7 +29,30 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+
+
+<div>
+  <b-card
+    title="Card Title"
+    img-src="https://picsum.photos/600/300/?image=25"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2"
+  >
+    <b-card-text>
+      Some quick example text to build on the card title and make up the bulk of the card's content.
+    </b-card-text>
+
+    <b-button href="#" variant="primary">Go somewhere</b-button>
+  </b-card>
+</div>
+
   </div>
+
+
+
 </template>
 
 <script>
