ca51abb178e06960c4f06b3c60c46a7d6d84fa7e
nunof <nunofag9@hotmail.com>
2019-12-13 16:19:00 +0000

    dsadsa

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
       <top-header></top-header>
-    <div id="nav">
-      <!-- <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> -->
-    </div>
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div> -->
     <router-view />
   </div>
 </template>
