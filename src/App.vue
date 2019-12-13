52cb7afa259c3c28c6feea11495baace678e440b
BrunoJusta <brunomajusta@gmail.com>
2019-12-13 13:57:29 +0000

    aves focus

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <div id="nav">
       <top-header></top-header>
+    <div id="nav">
       <!-- <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> -->
     </div>
