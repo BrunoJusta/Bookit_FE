ba7df832687e38b61aeaf5d40c6fc69ffea4875f
nunof <nunofag9@hotmail.com>
2020-01-11 20:29:13 +0000

    settings open

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -17,7 +17,9 @@
                     <p id="nameTxt">{{getFullName}}</p>
                 </div>
                 <div class="col-sm-8" id="optionsColumn">
-                    <b-img src="../assets/settingsIcon.png" id="settingsImg"></b-img>
+                    <a v-on:click="displaySettings()" class="options border-0">
+                        <b-img v-bind:src="settingsOn" id="settingsImg"></b-img>
+                    </a>
                     <b-button v-on:click="displayNotifications()" class="options border-0"
                         v-bind:style="{fontWeight: notificFont, color: notifiColor}">
                         Notificações</b-button>
@@ -33,91 +35,133 @@
 
 
         <!-- MOSTRA AS RESERVAS -->
-        <div v-bind:style="{display: showBookings}">
-            <div class="container" style="justify-content: center;">
-                <b-button v-on:click="displayEvents()" v-bind:style="{fontWeight: eventsFont}" style="fontSize: 20px;"
-                    class="bookingOptions border-0">Eventos & Catering
-                </b-button>
-                <b-button v-on:click="displayAreas()" v-bind:style="{fontWeight: AreasFont}" style="fontSize: 20px;"
-                    class="bookingOptions border-0">
-                    Espaços</b-button>
-                <b-button v-on:click="displayWorkshops()" v-bind:style="{fontWeight: WorkshopsFont}"
-                    style="fontSize: 20px;" class="bookingOptions border-0">Workshops
-                </b-button>
-            </div>
+        <div class="container">
+            <div v-bind:style="{display: showBookings}">
+                <div class="container" style="justify-content: center;">
+                    <b-button v-on:click="displayEvents()" v-bind:style="{fontWeight: eventsFont}"
+                        style="fontSize: 20px;" class="bookingOptions border-0">Eventos & Catering
+                    </b-button>
+                    <b-button v-on:click="displayAreas()" v-bind:style="{fontWeight: AreasFont}" style="fontSize: 20px;"
+                        class="bookingOptions border-0">
+                        Espaços</b-button>
+                    <b-button v-on:click="displayWorkshops()" v-bind:style="{fontWeight: WorkshopsFont}"
+                        style="fontSize: 20px;" class="bookingOptions border-0">Workshops
+                    </b-button>
+                </div>
 
-            <!-- CARDS DOS EVENTOS -->
-            <div class="container" v-bind:style="{display: showEvents}">
-                <div class="row" v-if="this.bookings.length !== 0">
-                    <div class="col-sm-3" v-for="k in filteredBookings" :key="k.id" style="padding-top: 20px;">
-                        <b-card no-body class="overflow-hidden" style="max-width: 16rem;" :img-src="k.kitImg">
-                            <b-card-body :title="k.kitName + ' - ' + k.kitType">
-                                <b-card-text style="margin: auto;">
-                                    <b>Data:</b> {{k.date}}
-                                </b-card-text>
-                                <b-card-text style="margin: auto;">
-                                    <b>Hora:</b> {{k.duration}}
-                                </b-card-text>
-                                <b-card-text style="margin: auto;">
-                                    <b>Local:</b> {{k.location}}
-                                </b-card-text>
-                            </b-card-body>
-                        </b-card>
+                <!-- CARDS DOS EVENTOS -->
+                <div class="container" v-bind:style="{display: showEvents}">
+                    <div class="row" v-if="this.bookings.length !== 0">
+                        <div class="col-sm-3" v-for="k in filteredBookings" :key="k.id" style="padding-top: 20px;">
+                            <b-card no-body class="overflow-hidden" style="max-width: 16rem;" :img-src="k.kitImg">
+                                <b-card-body :title="k.kitName + ' - ' + k.kitType">
+                                    <b-card-text style="margin: auto;">
+                                        <b>Data:</b> {{k.date}}
+                                    </b-card-text>
+                                    <b-card-text style="margin: auto;">
+                                        <b>Hora:</b> {{k.duration}}
+                                    </b-card-text>
+                                    <b-card-text style="margin: auto;">
+                                        <b>Local:</b> {{k.location}}
+                                    </b-card-text>
+                                </b-card-body>
+                            </b-card>
+                        </div>
+                    </div>
+                    <div v-else>
+                        <br>
+                        <p>NÃO EXISTEM RESERVAS FEITAS!</p>
                     </div>
                 </div>
-                <div v-else>
-                    <br>
-                    <p>NÃO EXISTEM RESERVAS FEITAS!</p>
+
+                <!-- CARDS DOS ESPAÇOS -->
+                <div class="container" v-bind:style="{display: showAreas}">
+                    <div class="row" v-if="this.areas.length !== 0">
+                        <div class="col-sm-3" v-for="k in filteredAreas" :key="k.id" style="padding-top: 20px;">
+                            <b-card no-body class="overflow-hidden" style="max-width: 16rem;" :img-src="k.areaImg">
+                                <b-card-body :title="k.areaName">
+                                    <b-card-text style="margin: auto;">
+                                        Data: {{k.date}}
+                                    </b-card-text>
+                                    <b-card-text style="margin: auto;">
+                                        Hora: {{k.duration}}
+                                    </b-card-text>
+                                </b-card-body>
+                            </b-card>
+                        </div>
+                    </div>
+                    <div v-else>
+                        <br>
+                        <p>NÃO EXISTEM RESERVAS DE ESPAÇOS FEITAS!</p>
+                    </div>
+                </div>
+
+                <!-- CARDS DOS WORKSHOPS -->
+                <div class="container" v-bind:style="{display: showWorkshops}">
+                    <div class="row" v-if="this.areas.length !== 0">
+                        <div class="col-sm-3" v-for="k in this.userWorkshops" :key="k.id" style="padding-top: 20px;">
+                            <b-card no-body class="overflow-hidden" style="max-width: 16rem;" :img-src="k.img">
+                                <b-card-body :title="k.name">
+                                    <b-card-text style="margin: auto;">
+                                        Data: {{k.date}}
+                                    </b-card-text>
+                                    <b-card-text style="margin: auto;">
+                                        Hora: {{k.time}}
+                                    </b-card-text>
+                                </b-card-body>
+                            </b-card>
+                        </div>
+                    </div>
+                    <div v-else>
+                        <br>
+                        <p>NÃO EXISTEM INSCRIÇÕES EM WORKSHOPS FEITAS!</p>
+                    </div>
                 </div>
             </div>
 
-            <!-- CARDS DOS ESPAÇOS -->
-            <div class="container" v-bind:style="{display: showAreas}">
-                <div class="row" v-if="this.areas.length !== 0">
-                    <div class="col-sm-3" v-for="k in filteredAreas" :key="k.id" style="padding-top: 20px;">
-                        <b-card no-body class="overflow-hidden" style="max-width: 16rem;" :img-src="k.areaImg">
-                            <b-card-body :title="k.areaName">
-                                <b-card-text style="margin: auto;">
-                                    Data: {{k.date}}
-                                </b-card-text>
-                                <b-card-text style="margin: auto;">
-                                    Hora: {{k.duration}}
-                                </b-card-text>
-                            </b-card-body>
-                        </b-card>
-                    </div>
-                </div>
-                <div v-else>
-                    <br>
-                    <p>NÃO EXISTEM RESERVAS DE ESPAÇOS FEITAS!</p>
-                </div>
+            <!-- MOSTRA AS NOTIFICACOES -->
+            <div v-bind:style="{display: showNotifications}">
+                <h1>TESTE PARA AS NOTIFICACOES</h1>
             </div>
 
             <!-- CARDS DOS WORKSHOPS -->
-            <div class="container" v-bind:style="{display: showWorkshops}">
-                <div class="row">
-                    <div class="col-sm-3" v-for="k in this.userWorkshops" :key="k.id" style="padding-top: 20px;">
-                        <b-card no-body class="overflow-hidden" style="max-width: 16rem;" :img-src="k.img">
-                            <b-card-body :title="k.name">
+            <div class="container" v-bind:style="{display: showSettings}">
+
+                <div>
+                    <b-card no-body>
+                        <b-tabs card>
+                            <b-tab title="Informações" active>
+                                <b-card-text>Tab contents 1</b-card-text>
+                            </b-tab>
+                            <b-tab title="Alterar Palavra-Passe">
+                                <b-card-text>Tab contents 2</b-card-text>
+                            </b-tab>
+                            <b-tab title="Atualizar Foto de Perfil">
+                                <b-card-text>Tab contents 2</b-card-text>
+                            </b-tab>
+                            <b-tab title="Atualizar Contacto">
+                                <b-card-text>Tab contents 2</b-card-text>
+                            </b-tab>
+                        </b-tabs>
+                    </b-card>
+                </div>
+                <!-- <div class="row">
+                    <div class="col-sm-3" style="padding-top: 20px;">
+                        <b-card no-body class="overflow-hidden" style="max-width: 16rem;">
+                            <b-card-body :title="'Informações'">
                                 <b-card-text style="margin: auto;">
-                                    Data: {{k.date}}
+                                    Data: {{getFullName}}
                                 </b-card-text>
                                 <b-card-text style="margin: auto;">
-                                    Hora: {{k.time}}
+                                    dsaoihdasoidhasoidjsa
                                 </b-card-text>
                             </b-card-body>
                         </b-card>
                     </div>
-                </div>
+                </div> -->
             </div>
         </div>
 
-        <!-- MOSTRA AS NOTIFICACOES -->
-        <div v-bind:style="{display: showNotifications}">
-            <h1>TESTE PARA AS NOTIFICACOES</h1>
-        </div>
-        <br>
-        <br>
     </div>
 </template>
 
@@ -134,11 +178,13 @@
                 userEmail: "",
                 showBookings: "block", //mostrar reservas
                 showNotifications: "none", //mostrar notificaçoes
+                showSettings: "none", //mostrar as definiçoes
                 showEvents: "block", //mostrar eventos dentro das reservas
                 showAreas: "none", //mostrar areas dentro das reservas
                 showWorkshops: "none", //mostrar workshops dentro das reservas
                 bookingsColor: "#B91C3B", //muda a cor da fonte escolhida                   
                 notifiColor: "black", //muda a cor da fonte escolhida
+                settingsOn: require('../assets/settingsIcon.png'), //imagem das definições
                 bookingsFont: "bold", //muda a cor da fonte escolhida                   
                 notificFont: "normal", //muda a cor da fonte escolhida
                 eventsFont: "bold",
@@ -209,18 +255,32 @@
             displayBookings() {
                 this.showBookings = "block"
                 this.showNotifications = "none"
+                this.showSettings = "none"
                 this.bookingsColor = "#B91C3B"
                 this.notifiColor = "black"
                 this.bookingsFont = "bold"
                 this.notificFont = "normal"
+                this.settingsOn = require('../assets/settingsIcon.png')
             },
             displayNotifications() {
                 this.showBookings = "none"
                 this.showNotifications = "block"
+                this.showSettings = "none"
                 this.bookingsColor = "black"
                 this.notifiColor = "#B91C3B"
                 this.bookingsFont = "normal"
                 this.notificFont = "bold"
+                this.settingsOn = require('../assets/settingsIcon.png')
+            },
+            displaySettings() {
+                this.showBookings = "none"
+                this.showNotifications = "none"
+                this.showSettings = "block"
+                this.bookingsColor = "black"
+                this.notifiColor = "black"
+                this.bookingsFont = "normal"
+                this.notificFont = "normal"
+                this.settingsOn = require('../assets/settingsIconRed.png')
             }
         },
         computed: {
@@ -246,7 +306,9 @@
 
 <style lang="scss" scoped>
     #perfilContainer {
-        box-shadow: 0px 0px 2px;
+        -webkit-box-shadow: 0px 0px 6px 2px rgba(0, 0, 0, 0.12);
+        -moz-box-shadow: 0px 0px 6px 2px rgba(0, 0, 0, 0.12);
+        box-shadow: 0px 0px 6px 2px rgba(0, 0, 0, 0.12);
     }
 
     .options {
@@ -267,7 +329,8 @@
     }
 
     #settingsImg {
-        margin-top: 15px;
+        margin-top: -107px;
+        margin-right: -25px;
         width: 50px;
         float: right;
     }
