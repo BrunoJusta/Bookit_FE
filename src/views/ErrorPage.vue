3c8d33d25ba06e5f3f8d681b48253e9ce882c359
BrunoJusta <brunomajusta@gmail.com>
2020-01-21 16:08:16 +0000

    hh

---
+++ src/views/ErrorPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <img src="../assets/bookit_BLUE.svg" alt="" srcset="">
+        <h1>ERRO ERRO ERRO</h1>
+        <h1>ESTA É A PÁGINA DE ERRO</h1>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
