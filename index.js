0697e64824187b8c640662ff05198e247841d8ef
BrunoJusta <brunomajusta@gmail.com>
2020-06-16 13:23:20 +0100

    deploy

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var app = express();
-var path = require('path');
+// var path = require('path');
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(__dirname + "/dist/"));
 app.use("/assets", express.static(__dirname + '/assets'));
 
 // viewed at based directory http://localhost:8080/
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+app.get(/.*/, function (req, res) {
+  res.sendFile(__dirname + '/dist/index.html');
 });
 
 app.listen(process.env.PORT || 8080);
