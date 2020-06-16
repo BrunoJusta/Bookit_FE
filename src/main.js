a923e5ffd08e3f6788cc8596dd48fc765ab93873
BrunoJusta <brunomajusta@gmail.com>
2020-06-16 13:04:20 +0100

    d

--- src/main.js
+++ src/main.js
@@ -15,16 +15,3 @@ new Vue({
   render: h => h(App)
 }).$mount('#app')
 
-var express = require('express');
-var app = express();
-var path = require('path');
-
-app.use(express.static(path.join(__dirname)));
-app.use("/assets", express.static(__dirname + '/assets'));
-
-// viewed at based directory http://localhost:8080/
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + 'views/index.html'));
-});
-
-app.listen(process.env.PORT || 8080);
