3296f819fbf20325f6fa6ae1c900ede5428032b6
BrunoJusta <brunomajusta@gmail.com>
2020-06-17 21:58:07 +0100

    Deploy github

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# abortar no caso de erros
+set -e
+# build
+npm run build
+# navegar para a pasta com o conteúdo compilado
+cd dist
+git init
+git add -A 
+git commit -m 'deploy'
+git push -f https://github.com/BrunoJusta/_bookit.git master
+cd -
